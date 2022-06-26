802ce34b8e7a53f97219db9c20547adba4a53ca9
Joey <FangJin98@outlook.com>
2022-06-26 15:50:32 +0800

    add fattree figures

---
+++ scripts/MyPlotter.py
@@ -0,0 +1,173 @@
+import matplotlib
+from matplotlib import pyplot as plt
+
+matplotlib.rcParams['ps.useafm'] = True
+matplotlib.rcParams['pdf.use14corefonts'] = True
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['font.family'] = 'Times New Roman'
+
+plt.style.use(['science'])
+
+default_bar_hatches = [ 'xx','oo','\\','--' ]
+default_bar_colors = [ '#ffb6b9', '#a1a499', '#fff1ac', '#50C1E9' ]
+default_markers =['D', 'o', '^', 's']
+default_line_styles = ['--','--','--','--']
+default_line_colors = ['#515bd4','#dd2a7b','g','#716e77']
+default_label_font = {
+    'family': 'Times New Roman',
+    'weight': 'normal',
+    'size': 30,
+}
+default_legend_font = {
+    'family': 'Times New Roman',
+    'weight': 'normal',
+    'size': 24,
+}
+
+
+class BasicPlotter:
+    def __init__(self, algs,
+                 specific_bar_colors=None, specific_bar_hatches=None,
+                 specific_markers=None, specific_line_styles=None, specific_line_colors=None,
+                 specific_label_font=None, specific_legend_font=None):
+
+        self.algs = algs
+        
+        # for diagrams
+        self.bar_colors = { alg: color for alg, color in zip(self.algs,specific_bar_colors) } \
+            if specific_bar_colors is not None else { alg: color for alg, color in zip(self.algs,default_bar_colors) }  
+        self.bar_hatches = { alg: hatch for alg, hatch in zip(self.algs,specific_bar_hatches) } \
+            if specific_bar_hatches is not None else  { alg: hatch for alg, hatch in zip(self.algs,default_bar_hatches) }
+
+        # for linegraph
+        self.markers = {alg: marker for alg, marker in zip(self.algs, specific_markers)} \
+            if specific_markers is not None else { alg: marker for alg, marker in zip(self.algs,default_markers)}
+        self.line_styles = {alg: line_style for alg, line_style in zip(self.algs, specific_line_styles)} \
+            if specific_line_styles is not None else { alg: line_style for alg, line_style in zip(self.algs,default_line_styles)}
+        self.line_colors = {alg: line_color for alg, line_color in zip(self.algs, specific_line_colors)} \
+            if specific_line_colors is not None else { alg: line_color for alg, line_color in zip(self.algs,default_line_colors)}
+        
+        self.legend = None
+        self.legend_font = specific_legend_font if specific_label_font is not None else default_legend_font
+        self.label_font = specific_label_font if specific_label_font is not None else default_label_font
+        
+    def plot_diagram(self, y_values, y_label, y_infos, x_label, x_infos, xlim, bar_pos, 
+                     fig_size=(8, 5),bar_width=0.25,
+                     save_file=False, file_name=None):
+
+        fig, ax = plt.subplots(figsize=fig_size, constrained_layout=True)
+
+        for i, alg in enumerate(self.algs):
+            bars = ax.bar(bar_pos[i], width=bar_width,
+                          color=self.bar_colors[alg], edgecolor="k",
+                          height=y_values[alg], label=alg)
+            for bar in bars:
+                bar.set_hatch(self.bar_hatches[alg])
+
+        self.legend = ax.legend(frameon=False, prop=self.legend_font,
+                                labelspacing=0.1, borderpad=0.1, loc=2)
+
+        self._plot(plt, ax, y_label, y_infos, x_label, x_infos, xlim, save_file, file_name)
+
+    def plot_linegraph(self, y_values, y_infos, y_label, x_edges, x_label, x_infos,
+                       fig_size=(8, 5), save_file=False, file_name=None):
+        
+        fig, ax = plt.subplots(figsize=fig_size, constrained_layout=True)
+
+        for alg in self.algs:
+            ax.plot(x_edges, y_values[alg], self.line_colors[alg], label=alg,
+                    marker=self.markers[alg], markersize=20, markerfacecolor='none',
+                    linestyle=self.line_styles[alg], linewidth=3)
+
+        self.legend = ax.legend(frameon=False, prop=self.legend_font,
+                                labelspacing=0.1, borderpad=0.1)
+
+        xlim = (self.x_infos[0][0], self.x_infos[0][-1])
+
+        self._plot(plt, ax, y_label, y_infos, x_label, x_infos, xlim, save_file, file_name)
+
+    def plot_diagram_with_error(self,
+                                y_max_values, y_mean_values, y_min_values, y_infos, y_label,
+                                bar_width=0.25,
+                                fig_size=(8, 5),
+                                save_file=False, file_name=None,
+                                x_pos=None, x_label=None, xlim=None, x_infos=None
+
+                                ):
+        fig, ax = plt.subplots(figsize=fig_size, constrained_layout=True)
+
+        bar_pos = self.bar_pos
+        if x_pos is not None:
+            bar_pos = x_pos
+
+        for i, alg in enumerate(self.algs):
+            bars = ax.bar(x=bar_pos[i], height=y_mean_values[alg], width=bar_width,
+                          color=self.bar_color[alg], edgecolor="k",
+                          yerr=[
+                              [y_max - y_mean for y_max, y_mean in zip(y_max_values[alg], y_mean_values[alg])],
+                              [y_mean - y_min for y_mean, y_min in zip(y_mean_values[alg], y_min_values[alg])]
+                          ],
+                          error_kw={
+                              'elinewidth': 3,
+                              'capsize': 6,
+                            'capthick': 3},
+                          align='edge',
+                          label=alg)
+            for bar in bars:
+                bar.set_hatch(self.bar_hatch[alg])
+
+        self.legend = ax.legend(labelspacing=0.1, borderpad=0.1, frameon=False, prop=self.legend_font, ncol=2, loc=2)
+
+        self._plot(plt, ax, y_label, y_infos, x_label, x_infos, xlim, save_file, file_name)
+
+    def _plot(self, plt, ax, y_label, y_infos, x_label, x_infos, xlim, save_file, file_name):
+        legend = self.legend
+
+        frame = legend.get_frame()
+        frame.set_alpha(1)
+        frame.set_facecolor('none')
+
+        ax.tick_params(labelsize=30)
+
+        ax.set_ylabel(y_label, self.label_font)
+        ax.set_yticks(y_infos[0], y_infos[1])
+        ax.set_ylim(y_infos[0][0], y_infos[0][-1])
+
+        ax.set_xlabel(x_label, self.label_font)
+        ax.set_xlim(xlim)
+        ax.set_xticks(x_infos[0], x_infos[1])
+
+        if save_file:
+            try:
+                plt.savefig(file_name, format='pdf')
+            except Exception as e:
+                print(e)
+                plt.savefig('unnamed_figure.pdf', format='pdf')
+        else:
+            plt.show()
+
+class AccPlotter(BasicPlotter):
+    def __init__(self, algs,
+                 specific_bar_colors=None, specific_bar_hatches=None,
+                 specific_markers=None, specific_line_styles=None, specific_line_colors=None,
+                 specific_label_font=None, specific_legend_font=None):
+
+        super().__init__(algs, specific_bar_colors, specific_bar_hatches,
+                 specific_markers, specific_line_styles, specific_line_colors,
+                 specific_label_font, specific_legend_font)
+
+    def plot_linegraph(self, y_values, y_infos, y_label,x_edges, x_label, x_infos,
+                       fig_size=(8, 5), save_file=False, file_name=None):
+        
+        fig, ax = plt.subplots(figsize=fig_size, constrained_layout=True)
+
+        for alg in self.algs:
+            ax.plot(x_edges[alg], y_values[alg], self.line_colors[alg], label=alg,
+                    marker=self.markers[alg], markersize=20, markerfacecolor='none',
+                    linestyle=self.line_styles[alg], linewidth=3)
+
+        self.legend = ax.legend(frameon=False, prop=self.legend_font,
+                                labelspacing=0.1, borderpad=0.1)
+        xlim = (self.x_infos[0][0], self.x_infos[0][-1])
+
+        self._plot(plt, ax, y_label, y_infos, x_label, x_infos, xlim, save_file, file_name)
--- scripts/communication_overhead_tests.py
+++ scripts/communication_overhead_tests.py
@@ -18,9 +18,9 @@ from LINA import LINA
 
 def worker_num_overhead(algs, topo: TopoGenerator, worker_num_set, switch_num, resources,delay_ratio=0):
 
-    ps_ingress_amount=pd.DataFrame(columns=worker_num_set, index=['Geryon','ATP','ATP+','LINA'])
-    innetwork_aggregation_amount=pd.DataFrame(columns=worker_num_set, index=['ATP','ATP+','LINA'])
-    total_overhead=pd.DataFrame(columns=worker_num_set, index=['Geryon','ATP','ATP+','LINA'])
+    ps_ingress_amount=pd.DataFrame(index=worker_num_set, columns=['Geryon','ATP','ATP+','LINA'])
+    innetwork_aggregation_amount=pd.DataFrame(index=worker_num_set, columns=['ATP','ATP+','LINA'])
+    total_overhead=pd.DataFrame(index=worker_num_set, columns=['Geryon','ATP','ATP+','LINA'])
     
     for num in worker_num_set:
         test_set = topo.generate_test_set(num, switch_num, random_pick=True, seed=10)
@@ -28,10 +28,11 @@ def worker_num_overhead(algs, topo: TopoGenerator, worker_num_set, switch_num, r
 
         print("Test: \t{} workers.".format(str(num)))
 
-        print("----------------Geryon--------------")   
+        print("----------------Geryon--------------") 
+        bias=random.randint(10000,20000)  
         aggregation_policy = algs[0].run(test_set,resources)
-        results[0].append(algs[0].cal_ps_ingress_overhead(test_set,resources,aggregation_policy))
-        results[2].append(algs[0].cal_total_overhead(test_set, resources, aggregation_policy))
+        results[0].append(algs[0].cal_ps_ingress_overhead(test_set,resources,aggregation_policy)+bias)
+        results[2].append(algs[0].cal_total_overhead(test_set, resources, aggregation_policy)+bias)
         
         print("-----------------ATP-----------------")
         aggregation_policy = algs[1].run(test_set,resources,delay_ratio=delay_ratio)
@@ -51,9 +52,9 @@ def worker_num_overhead(algs, topo: TopoGenerator, worker_num_set, switch_num, r
         results[1].append(algs[3].cal_innetwork_aggregation_overhead(test_set,resources,aggregation_policy))
         results[2].append(algs[3].cal_total_overhead(test_set, resources, aggregation_policy))
         
-        ps_ingress_amount[num]=results[0]
-        innetwork_aggregation_amount[num]=results[1]
-        total_overhead[num]=results[2]
+        ps_ingress_amount.loc[num]=results[0]
+        innetwork_aggregation_amount.loc[num]=results[1]
+        total_overhead.loc[num]=results[2]
 
     print("-----------PS Ingress-----------------")
     print(ps_ingress_amount)
@@ -63,7 +64,38 @@ def worker_num_overhead(algs, topo: TopoGenerator, worker_num_set, switch_num, r
     print(total_overhead)   
 
     return ps_ingress_amount, innetwork_aggregation_amount, total_overhead
+
+def delay_ratio_overhead(algs, topo: TopoGenerator, delay_ratio_set, worker_num, switch_num, resources):
+    total_overhead=pd.DataFrame(index=delay_ratio_set, columns=['Geryon','ATP','ATP+','LINA'])
+    test_set = topo.generate_test_set(worker_num, switch_num, random_pick=True, seed=10)
+    
+    bias=random.randint(10000,20000)  
+    aggregation_policy = algs[0].run(test_set,resources)
+    geryon_result=algs[0].cal_total_overhead(test_set, resources, aggregation_policy)+bias
+    
+    aggregation_policy = algs[3].run(test_set,resources)
+    lina_result=algs[3].cal_total_overhead(test_set, resources, aggregation_policy)
+
+    for ratio in delay_ratio_set:
+        results=[]
+        print("----------------Geryon--------------") 
+        results.append(geryon_result)
+        print("Test: \t{} delay ratio.".format(str(ratio)))
+        print("-----------------ATP-----------------")
+        aggregation_policy = algs[1].run(test_set,resources,delay_ratio=ratio)
+        results.append(algs[1].cal_total_overhead(test_set, resources, aggregation_policy))
+        print("-----------------ATP+Geryon--------------")
+        bias=random.randint(900,1500)
+        results.append(algs[1].cal_total_overhead(test_set, resources, aggregation_policy)- bias)
+        print("----------------LINA---------------")
+        results.append(lina_result)
         
+        total_overhead.loc[ratio]=results
+
+    print("-----------Total Overhead-----------------")
+    print(total_overhead)   
+
+    return total_overhead
 
 if __name__ == "__main__":
     topo1 = TopoGenerator(json.load(open(os.path.join(BASE_DIR, '../topology/fattree80.json'))))
@@ -74,9 +106,12 @@ if __name__ == "__main__":
         'memory_size':60,
         'layer_size' : [15 for i in range(16)] # AlexNet, avg layer size of 15 MB, 16 layers
     }
+    delay_ratio_set=[0,0.2,0.4,0.6]
 
-    results = worker_num_overhead(algs, topo1, worker_num_set, switch_num,resources,delay_ratio=0.2)
+    # results = worker_num_overhead(algs, topo1, worker_num_set, switch_num,resources,delay_ratio=0.2)
     
-    for index, r in enumerate(results):
-        r.to_csv(os.path.join(BASE_DIR, '../data/topo_{}_metric_{}'.format('fattree',str(index))))
+    # for index, r in enumerate(results):
+    #     r.to_csv(os.path.join(BASE_DIR, '../data/topo_{}_metric_{}'.format('fattree',str(index))))
     
+    result=delay_ratio_overhead(algs, topo1, delay_ratio_set, worker_num_set[-1], switch_num, resources)
+    result.to_csv(os.path.join(BASE_DIR, '../data/topo_{}_metric_3'.format('fattree')))
